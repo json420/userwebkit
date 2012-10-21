9fb9d88fce12f2d13aa2bd10c774992113a8dfcc
Jason Gerard DeRose <jderose@novacut.com>
2012-10-21 16:55:19 -0600

    Bumped version to 12.11

--- setup.py
+++ setup.py
@@ -70,7 +70,7 @@ setup(
     name='userwebkit',
     description='so WebKitGtk apps can to talk to a usercouch',
     url='https://launchpad.net/userwebkit',
-    version='12.10.0',
+    version='12.11.0',
     author='Jason Gerard DeRose',
     author_email='jderose@novacut.com',
     license='LGPLv3+',
