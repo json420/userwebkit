f26d5f4d137ab2a8dab2551493faab6632f8dffe
Jason Gerard DeRose <jderose@novacut.com>
2013-02-20 22:13:35 -0700

    Require Python >= 3.3

--- setup.py
+++ setup.py
@@ -26,8 +26,8 @@ Install `userwebkit`.
 """
 
 import sys
-if sys.version_info < (3, 2):
-    sys.exit('ERROR: UserWebKit requires Python 3.2 or newer')
+if sys.version_info < (3, 3):
+    sys.exit('ERROR: UserWebKit requires Python 3.3 or newer')
 
 from distutils.core import setup
 from distutils.cmd import Command
