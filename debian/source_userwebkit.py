3513464ce323437d949a2ee66d9b4ff376c5e4f5
Jason Gerard DeRose <jderose@novacut.com>
2013-02-20 23:43:21 -0700

    remove userwebkit-crashdb.conf, providing the same in source_userwebkit.py

--- debian/source_userwebkit.py
+++ debian/source_userwebkit.py
@@ -1,10 +1,9 @@
 """
-apport package hook for userwebkit.
+Apport package hook for userwebkit (requires Apport 2.5 or newer).
 
 (c) 2012 Novacut Inc
 Author: Jason Gerard DeRose <jderose@novacut.com>
 """
 
 def add_info(report):
-    report['CrashDB'] = 'userwebkit'
-
+    report['CrashDB'] = "{'impl': 'launchpad', 'project': 'userwebkit'}"
