bbfd9d78a1b2540210f4a3c3eafefda78a04bb72
Jason Gerard DeRose <jderose@novacut.com>
2013-10-27 17:11:39 -0600

    Bumped version to 13.11

--- userwebkit/__init__.py
+++ userwebkit/__init__.py
@@ -37,7 +37,7 @@ from dbus.mainloop.glib import DBusGMainLoop
 from gi.repository import GLib, GObject, Gtk, WebKit
 
 
-__version__ = '13.10.0'
+__version__ = '13.11.0'
 APPS = '/usr/share/couchdb/apps/'
 GObject.threads_init()
 DBusGMainLoop(set_as_default=True)
